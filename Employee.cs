40592aae2cba85ec3021a115cd66d25467eed917
ASISH DHARAMUKKULA <asishdharamukkula@gmail.com>
2017-07-28 22:08:55 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationCRUD.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int id { get; set; }
+        public String Firstname { get; set; }
+        public String Lastnamee { get; set; }
+        public String Address { get; set; }
+       
+    }
+}
